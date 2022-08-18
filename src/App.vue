4e9548e1f42f9d60d1781f8f3fc0fac221fcf217
Juan Rios <juanesrios13@gmail.com>
2022-08-18 11:45:14 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import DrawflowView from "./views/DrawflowView.vue";
+</script>
+
+<template>
+  <div class="principal1">
+    <DrawflowView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/DrawflowView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app1">
+    <draw-flow />
+  </div>
+</template>
+
+<script>
+import DrawFlow from "../components/DrawFlow.vue";
+("");
+export default {
+  name: "App",
+  components: { DrawFlow },
+};
+</script>
+
+<style>
+#app1 {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
